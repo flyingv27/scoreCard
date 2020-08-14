da5b6719c4c75182a8c3fc42ad01310c8e96cd20
John <John@Johnui-MacBook-Pro.local>
2020-08-14 14:26:20 +0900

    20200814_1

--- main.js
+++ main.js
@@ -1,7 +1,18 @@
 var http = require('http');
 var fs = require('fs');
+var urlmodule = require('url');
+
+var querystring = require('querystring');
+
 var app = http.createServer(function(request,response){
+    
+    var parsedUrl = urlmodule.parse(request.url);
+    console.log(parsedUrl.pathname);
+
+    var parsedQuery = querystring.parse(parsedUrl.query, '&', '=');
+    console.log(parsedQuery);
     var url = request.url;
+
     if(url === '/favicon.ico') {
         response.writeHead(200, {'Content-Type': 'image/x-icon'});
         response.end();
@@ -12,6 +23,10 @@ var app = http.createServer(function(request,response){
       url = '/index.html';
     }
     response.writeHead(200);
+    //response.end('End...');
+    //response.end(url);
+    console.log("========="+url);
+
     response.end(fs.readFileSync(__dirname + url));
 
 });
